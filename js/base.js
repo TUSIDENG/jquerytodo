ff05199e06d86ee34bd0cd89138454e93be48419
TUSIDENG <1561799709@qq.com>
2019-05-28 12:49:50 +0800

    完成任务删除

--- js/base.js
+++ js/base.js
@@ -1,6 +1,7 @@
 ;(function () {
     'use strict';
     var $form_add_task = $('.add-task'),
+    $delete_task,
     $task_list = $('.task-list'),
      task_list = {};
 
@@ -15,10 +16,22 @@
          if (!new_task.title) return;
         
          if (add_task(new_task)) {
-             $task_list.append(render_task_list(new_task));
+            render_task_list();
              $input.val(null);
          }
      });
+
+     /**
+      * 监听任务删除
+      */
+     function listen_task_delete() {
+        $delete_task.on('click', function() {
+            var $this = $(this);
+            var index = $this.parent().parent().data('index');
+            var tmp = confirm('确认删除?');
+            (tmp ? delete_task(index) : null) ? render_task_list() : null;
+        })
+     }
 
     function init() {
          task_list = store.get('task_list') || [];
@@ -33,21 +46,41 @@
         return true;
     }
 
+    /**
+     * 删除一条Task 
+     */
+    function delete_task(index) {
+        if (index ===undefined || !task_list[index]) return;
+        task_list.splice(index, 1);
+        store.set('task_list', task_list);
+        return true;
+    }
+
+    /**
+     * 渲染所有Task模板
+     */
     function render_task_list() {
         $task_list.empty();
         task_list.map(function(task, index) {
-            var $task_item_tpl = render_task_tpl(task);
-            $task_list.append($task_item_tpl);
+            var $task_item = render_task_item(task, index);
+            $task_list.append($task_item);
         });
+        $delete_task = $('.action.delete');
+        listen_task_delete();
     }
 
-    function render_task_tpl(data) {
-        var list_item_tpl = '<li class="task-item">' + 
+    /**
+     * 渲染单条Task模板
+     * @param {Task} data 
+     * @param {int} index 
+     */
+    function render_task_item(data, index) {
+        var list_item_tpl = '<li class="task-item" data-index="' + index + '">' +
         '<span><input type="checkbox" name="" id=""></span>' +
         '<span class="task-title">' + data.title + '</span>' +
         '<span class="fr">' +
             '<span class="action"> 详情</span>' +
-            '<span class="action"> 删除</span>' +
+            '<span class="action delete"> 删除</span>' +
         '</span>'
         '</li>';
 
