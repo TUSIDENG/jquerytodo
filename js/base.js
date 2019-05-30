f0107e2c111f109371e1ef8db2ffcd9fad0f3d8a
TUSIDENG <1561799709@qq.com>
2019-05-30 14:33:10 +0800

    任务详情展示

--- js/base.js
+++ js/base.js
@@ -1,7 +1,9 @@
 ;(function () {
     'use strict';
     var $form_add_task = $('.add-task'),
-    $delete_task,
+    $task_delete_trigger,
+    $task_detail = $('.task-detail'),
+    $task_detail_mask = $('.task-detail-mask'),
     $task_list = $('.task-list'),
      task_list = {};
 
@@ -25,13 +27,26 @@
       * 监听任务删除
       */
      function listen_task_delete() {
-        $delete_task.on('click', function() {
+        $task_delete_trigger.on('click', function() {
             var $this = $(this);
             var index = $this.parent().parent().data('index');
             var tmp = confirm('确认删除?');
             (tmp ? delete_task(index) : null) ? render_task_list() : null;
-        })
+        });
      }
+
+     /**
+      * 监听点击任务详情
+      */
+     $task_list.on('click', '.action.detail', function() {
+         var index = $('.task-item').index($(this).parent().parent());
+         show_task_detail(index);
+     })
+
+     /**
+      * 点击遮罩隐藏任务详情面板
+      */
+     $task_detail_mask.on('click', hide_task_detail);
 
     function init() {
          task_list = store.get('task_list') || [];
@@ -56,6 +71,22 @@
         return true;
     }
 
+    // 展示任务详情
+    function show_task_detail(index)
+    {
+        var tpl = render_task_detail(index);
+        $task_detail.html(tpl);
+        $task_detail.show();
+        $task_detail_mask.show();
+    }
+
+    // 隐藏任务详情
+    function hide_task_detail()
+    {
+        $task_detail.hide();
+        $task_detail_mask.hide();
+    }
+
     /**
      * 渲染所有Task模板
      */
@@ -65,7 +96,7 @@
             var $task_item = render_task_item(task, index);
             $task_list.append($task_item);
         });
-        $delete_task = $('.action.delete');
+        $task_delete_trigger = $('.action.delete');
         listen_task_delete();
     }
 
@@ -79,11 +110,32 @@
         '<span><input type="checkbox" name="" id=""></span>' +
         '<span class="task-title">' + data.title + '</span>' +
         '<span class="fr">' +
-            '<span class="action"> 详情</span>' +
+            '<span class="action detail"> 详情</span>' +
             '<span class="action delete"> 删除</span>' +
         '</span>'
         '</li>';
 
         return $(list_item_tpl);
+    }
+
+    /**
+     * 渲染任务详情
+     * @param {int} index 
+     */
+    function render_task_detail(index) {
+        var item = task_list[index];
+        var tpl = '<div class="title">' +
+        item.title + 
+    '</div>' +
+    '<div>' +
+        '<div class="desc">' +
+            '<textarea name="" value="' + item.desc + '" id=""></textarea>' +
+        '</div>' +
+    '</div>' +
+    '<div class="remind">' +
+        '<input type="date" value="' + item.remind + '" name="" id="">' +
+        '<!-- <button type="submit">submit</button> -->' +
+    '</div>'
+    return tpl;
     }
 })();
